c2138ca25be781061f8bfbdfe7e2bb96f477e462
Sinity <ezo.dev@gmail.com>
2024-09-29 19:40:07 +0200

    Bind caps->ESC via Interception-Tools service

--- modules/core/hardware.nix
+++ modules/core/hardware.nix
@@ -34,6 +34,17 @@
     };
   };
 
+  # make capslock key into escape key
+  services.interception-tools = {
+    enable = true;
+    udevmonConfig = ''
+      - JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.caps2esc}/bin/caps2esc -m 1 | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
+        DEVICE:
+          EVENTS:
+            EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
+    '';
+  };
+
   # periodically TRIM ssd storage devices
   services.fstrim.enable = true;
 
