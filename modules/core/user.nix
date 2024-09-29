b36ed217455bae7d7bea93bd43aeb0115394d467
Sinity <ezo.dev@gmail.com>
2024-09-29 20:29:19 +0200

    users immutable un-declaratively; set hashedPassword for root and my account

--- modules/core/user.nix
+++ modules/core/user.nix
@@ -17,12 +17,18 @@
       programs.home-manager.enable = true;
     };
   };
-
+  
+  users.mutableUsers = false;
   users.users.${username} = {
     isNormalUser = true;
-    description = "${username}";
     extraGroups = [ "networkmanager" "wheel" ];
     shell = pkgs.zsh;
+    hashedPassword = "$y$j9T$EE/EN4Ifgi/859ru4tQxQ.$4h7Cv885iNyHz3pjhjubW3DZqMVb9wLuAunKGKE1qFC";
   };
   nix.settings.allowed-users = [ "${username}" ];
+  users.users.root = {
+    shell = pkgs.zsh;
+    home = "/root";
+    hashedPassword = "$y$j9T$S.QFCdxUUIlKBf6jlfsvq.$HAW7J8VmGgLXS7lONDpKq/p4tZRzpooROQgeo6iXYu6"; 
+  };
 }
